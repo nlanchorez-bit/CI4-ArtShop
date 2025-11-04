96bd82afdbf1b29b1ef80bce33653079e91d6637
nikonikoniii <nlanchorez@fit.edu.ph>
2025-11-04 18:20:32 +0800

    refactor(auth): Updated Auth Controller with following functions and expose Routes
    - Login = Auth::login
    - Logout = Auth::logout
    - Sign Up = Auth::signup

--- backend/app/Config/Routes.php
+++ backend/app/Config/Routes.php
@@ -6,4 +6,11 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Users::index');
+
+$routes->get('login',  'Auth::login');
 $routes->post('login', 'Auth::login');
+
+$routes->post('logout', 'Auth::logout');
+
+$routes->get('signup',  'Auth::signup');
+$routes->post('signup', 'Auth::signup');
