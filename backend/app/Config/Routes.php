4b58c9000ebc63d3d5d7bbb946c48d86536d5bd8
nikonikoniii <nlanchorez@fit.edu.ph>
2025-11-06 00:43:01 +0800

    feat(dashboard): Added admin dashboard, and routing

--- backend/app/Config/Routes.php
+++ backend/app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Users::index');
+$routes->get('admin', 'Admin::index');
+$routes->get('shop', 'Shop::index');
+$routes->get('users', 'Users::index');
+$routes->get('requests', 'Requests::index');
